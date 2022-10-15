6ba201f6002c1965600cb54806d4862223b450b4
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-15 16:10:16 +0500

    apna comment

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -2,42 +2,51 @@ import { BsFacebook, BsGithub } from "react-icons/bs";
 import { AiFillTwitterCircle } from "react-icons/ai";
 
 const Login = () => {
-    return (
-        <>
-            <div className="leftDiv"></div>
+  return (
+    <>
+      <div className="leftDiv"></div>
 
-            <div className="rightDiv">
-
-                <form className="form">
-                    <h1>Login to continue</h1>
-
-                    <input type="email" name="email" id="" placeholder="Email" />
-                    <input type="password" name="password" id="" placeholder="password" />
-                    <button type="submit">LOGIN</button>
-
-                    <div className="flex">
-
-                        <div>
-                            <input type="checkbox" name="rememberMe" id="" />
-                            <label htmlFor="rememberMe">Remember me</label>
-                        </div>
-
-                        <a href="https://github.com/shehza-d/login">Forgot Password?</a>
-                    </div>
-
-
-                </form>
-                <p>or sign up using</p>
-                <div>
-                    <a href="#"><BsFacebook /></a>
-                    <a href="#"><AiFillTwitterCircle /></a>
-                    <a href="#"><BsGithub /></a>
-
-
-                </div>
+      <div className="rightDiv">
+        <form className="form">
+          <h1>Login to continue</h1>
+          <input type="email" name="email" id="email" placeholder="Email" />
+          <br />
+          <input
+            type="password"
+            name="password"
+            id=""
+            placeholder="Password"
+          />{" "}
+          <br />
+          <br />
+          <br />
+          <button type="submit">LOGIN</button>
+          <br />
+          <br />
+          <div className="flex">
+            <div>
+              <input type="checkbox" name="rememberMe" id="" />
+              <label htmlFor="rememberMe">Remember me</label>
             </div>
-        </>
-    );
-}
+
+            <a href="https://github.com/shehza-d/login">Forgot Password?</a>
+          </div>
+        </form>
+        <p>or sign up using</p>
+        <div className="icons">
+          <a href="https://github.com/shehza-d">
+            <BsGithub />
+          </a>
+          <a href="#">
+            <AiFillTwitterCircle />
+          </a>
+          <a href="#">
+            <BsFacebook />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Login;
